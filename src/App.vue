29c267f99fd05bd9b13e5a3d834ce7e1af38cacb
Scott <houshangwei@gmail.com>
2021-08-18 17:06:57 +0800

    Adjust style

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,8 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: lightgrey;
+  background-color: #2c3e50;
   width: 100%;
   height: 100%;
   display: flex;
